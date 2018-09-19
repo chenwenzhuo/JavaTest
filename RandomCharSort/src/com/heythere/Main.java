69e6e9f8e1bd4e44a2053ed98aa6348c4f748c49
chenwenzhuo <Chenwz1996@163.com>
2018-09-19 11:59:40 +0800

    add RandomCharSort

--- RandomCharSort/src/com/heythere/Main.java
+++ RandomCharSort/src/com/heythere/Main.java
@@ -1,8 +1,30 @@
 package com.heythere;
 
-public class Main {
+import java.util.Random;
 
+public class Main {
+    private Random random;
+
+    public Main() {
+        this.random = new Random();
+    }
+
+    public void getRandomChars(char[] chars) {
+        byte[] bytes = new byte[chars.length];
+        random.nextBytes(bytes);
+
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = (char) bytes[i];
+        }
+
+        for (char aChar : chars) {
+            System.out.print(aChar);
+        }
+    }
     public static void main(String[] args) {
-	// write your code here
+        Main m = new Main();
+
+        char[] someChars = new char[5];
+        m.getRandomChars(someChars);
     }
 }
